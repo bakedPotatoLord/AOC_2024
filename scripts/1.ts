943e916b2a8545a6f77675ebb3e657cb0702e56a
bakedpotatolord <jeffy9745@gmail.com>
2024-12-01 14:15:39 -0700

    complete day 1

--- scripts/1.ts
+++ scripts/1.ts
@@ -1,4 +1,27 @@
 import {getInput, numberSum} from './helpers'
 
-const raw = (await getInput(5))
+const raw = (await getInput(1))
 .split("\n")
+.map(l=>l.split("   "))
+
+const left = raw.map(l=>parseInt(l[0])).sort((a,b)=>b-a)
+const right = raw.map(l=>parseInt(l[1])).sort((a,b)=>b-a)
+
+const diffs = right.map((r,i)=>Math.abs(Number(r)-Number(left[i])))
+
+
+console.log("part 1:",numberSum(diffs)) 
+
+
+const occurs = new Map<number,number>()
+
+right.forEach((r,i)=>{
+  if(occurs.has(r)) occurs.set(r,occurs.get(r)+1)
+  else occurs.set(r,1)
+})
+
+const leftoccs = left.map(l=> l * (occurs.get(l) ?? 0))
+
+console.log(leftoccs)
+
+console.log("part 2:",numberSum(leftoccs))
