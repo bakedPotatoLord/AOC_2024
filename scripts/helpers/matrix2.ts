3684770c3844d3a6007128d7f0af529d5ac64cc9
bakedpotatolord <jeffy9745@gmail.com>
2024-12-29 19:36:05 -0700

    add basic functions

--- scripts/helpers/matrix2.ts
+++ scripts/helpers/matrix2.ts
@@ -1,10 +1,27 @@
+import { re } from "mathjs";
 
 
 export default class Matrix2<T> extends Array<Array<T>> {
   private height: number;
   private width: number;
+  private mx:T[][];
 
   constructor(mx:T[][] ) {
-    super();
+    super(mx.length);
+
+    this.height = mx.length;
+    this.width = mx[0].length;
+    this.mx = structuredClone(mx)
+    
+  }
+
+  get(i: number, j: number) {
+    if(!this.inBounds(i,j)) return undefined
+    return this.mx[i][j]
+  }
+
+
+  inBounds(i: number, j: number) {
+    return i >= 0 && i < this.height && j >= 0 && j < this.width
   }
 }
