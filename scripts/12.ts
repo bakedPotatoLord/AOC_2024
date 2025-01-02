127ff071c60d8cfd2f7ff2efbccb5f807e3f0857
bakedpotatolord <jeffy9745@gmail.com>
2025-01-02 12:25:10 -0700

    complete day 12

--- scripts/12.ts
+++ scripts/12.ts
@@ -1,5 +1,4 @@
-import {getInput, numberSum} from './helpers'
-type vec2 = [number, number]
+import {getInput, numberSum,v2,vec2} from './helpers'
 
 const raw = (await getInput(12))
 .split("\n")
@@ -11,19 +10,18 @@ const visited = structuredClone(raw).map(l => l.map(_ => false))
 const regionCosts = new Map<string, number>()
 const regionCosts2 = new Map<string, number>()
 
-
 for (let i = 0; i < raw.length; i++) {
   for (let j = 0; j < raw[i].length; j++) {
     if(visited[i][j]) continue
     const char = raw[i][j]
     let area = 1
-    const region:vec2[] = [[i,j]]
-    let queue:vec2[] = [[i,j]]
+    const region:vec2[] = [v2(i,j)]
+    let queue:vec2[] = [v2(i,j)]
     visited[i][j] = true
 
     while (queue.length > 0) {
-      const [x, y] = queue.pop()
-      const touching = gettouching(x, y)
+      let v = queue.pop()
+      const touching = v.neighbors4
       .filter(t => isOnScreen(t[0], t[1]) && raw[t[0]][t[1]] == char && !visited[t[0]][t[1]])
 
       touching.forEach(t => visited[t[0]][t[1]] = true)
@@ -32,30 +30,28 @@ for (let i = 0; i < raw.length; i++) {
       area += touching.length
       region.push(...touching)
 
-      //do sides calc here
       
-      for(let i = 0; i < touching.length; i++){ //iterate over rows
-
-        let last
-
-        let j=0
-        while(j < touching.length){ //iterate over columns
-          
-        }
-      }
-
     }
 
-    const regionSet = new Set(region.map(l => l.join(",")))
+    const regionSet = new Set(region.map(l => l.toHash()))
 
-    let perimeter = numberSum(region.map(([i,j]) => gettouching(i,j)
-    .filter(t =>! regionSet.has(t.join(","))).length))
+    let perimeter = numberSum(region.map((v) => v.neighbors4
+    .filter(t =>! regionSet.has(t.toHash())).length))
 
 
     // console.log("char", char, "area", area, "perimeter", perimeter)
 
+
+    const sides = calcBulk(regionSet)
+
     if(regionCosts.has(char)) regionCosts.set(char, regionCosts.get(char)+ (area* perimeter))
     else regionCosts.set(char, area* perimeter)
+  
+
+    console.log("char", char, "area", area, "sides", sides)
+    if(regionCosts2.has(char)) regionCosts2.set(char, regionCosts2.get(char)+ (area*sides))
+    else regionCosts2.set(char, (area*sides))
+    
   }
 }
 
@@ -64,14 +60,96 @@ function isOnScreen(i: number, j: number) {
   return i >= 0 && i < raw.length && j >= 0 && j < raw[i].length
 }
 
-function gettouching(i: number, j: number) {
-  return <vec2[]>[ [i,j+1],[i,j-1],[i+1,j],[i-1,j] ]
-}
+
 
 // console.log(regionCosts)
 
 console.log("part 1:", numberSum(Array.from(regionCosts.values())))  
 
+console.log("part 2:", numberSum(Array.from(regionCosts2.values())))
 
 
- 
+//returns number of sides from hash set
+function calcBulk(hashes: Set<string>) {
+
+  let sideCt = 0
+  const mxc = structuredClone(raw) // matrix copy
+
+  
+
+  //do sides calc here
+        
+  for(let r = 0; r < 2; r++){ //rotate the matrix 90 degrees
+    // console.log(char)
+    
+      // I'm only gonna be searching horisontally and then vertically
+      //add to the map if you go from non-char to char
+      
+      //searching top row
+      let f = false
+      for(let i = 0; i < mxc[0].length; i++){
+        let isChar = inHashes(v2(0,i))
+        if(isChar && !f) sideCt++
+        f = isChar
+      } 
+
+      //searching bottom row
+      f = false
+      for(let i = 0; i < mxc[mxc.length-1].length; i++){
+        let isChar = inHashes(v2(mxc.length-1,i))
+        if(isChar && !f ) sideCt++
+        f = isChar
+      }
+      //search all inside rows
+      for(let i = 0; i < mxc.length-1; i++){
+        
+        let ft = false
+        let fb = false
+        let madeFence = false
+        for(let j = 0; j < mxc[mxc.length-1].length; j++){//searching both rows
+          let t = inHashes(v2(i,j))
+          let b = inHashes(v2(i+1,j))
+          // console.log(a,b)
+          let needFence = ( t !== b) 
+          //sidect increaces if jump from t xor b to ! (t xor b) 
+          // or if jump from t to !t and b to !b
+          if( needFence && (!madeFence || (t == !ft && b == !fb))  ) sideCt++
+          ft = t
+          fb = b
+          madeFence = needFence
+        }
+
+      }
+    
+    rotateMatrix(mxc)
+  }
+
+  return sideCt
+
+  function inHashes(v:vec2){
+    return hashes.has(v.toHash())
+  }
+
+  function rotateMatrix(matrix: any[][]) {
+    const n = matrix.length;
+
+    // Transpose the matrix
+    for (let i = 0; i < n; i++) {
+      for (let j = i; j < n; j++) {
+        const temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+
+    // Reverse each row
+    for (let i = 0; i < n; i++) {
+      matrix[i].reverse();
+    }
+  }
+
+
+}
+
+// console.log("part 2:", sum)
+
