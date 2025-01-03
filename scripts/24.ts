fa02167f6b4b4008d9266c0ef72477da8c3e7a2c
bakedpotatolord <jeffy9745@gmail.com>
2025-01-03 15:24:06 -0700

    did some thinking, but didnt change much

--- scripts/24.ts
+++ scripts/24.ts
@@ -36,7 +36,6 @@ function rec(gate:string){
     let res = rec(a) && rec(b)
     memo.set(gate,res)
     return res
-    
   }else if(op == "XOR"){
     let res = rec(a) !== rec(b)
     memo.set(gate,res)
@@ -46,9 +45,45 @@ function rec(gate:string){
 
 // console.log(memo)
 
-let zbits = memo.entries().filter(([k,v])=>k.startsWith("z")).toArray()
-.sort((a,b)=>parseInt(b[0].substring(1))-parseInt(a[0].substring(1)))
-.map(([k,v])=>Number(v))
-.join("")
+function getNum(startChar:string){
+  let bits = Array.from(memo.entries())
+  .filter(([k,v])=>k.startsWith(startChar))
+  .sort((a,b)=>parseInt(b[0].substring(1))-parseInt(a[0].substring(1)))
+  .map(([k,v])=>Number(v))
+  .join("")
 
-console.log("p1:",parseInt(zbits,2))
+  return parseInt(bits,2)
+}
+
+let z = getNum("z")
+console.log("p1:",z)
+
+let x = getNum("x")
+let y = getNum("y")
+
+let sum = x+y
+
+console.log(z.toString(2),"z")
+console.log(sum.toString(2),"x+y")
+
+console.log((z^sum).toString(2),"z^x+y")
+
+//313-92 = 221 gates
+
+// two gate swaps
+//first swap has 221*220  = 48620 possible swaps
+// second swap has 219*218 = 47742 possible swaps
+
+// total = 48620*47742 = 2_321_216_040 
+// I'd have to run the simulation 2 billion times.
+
+//not feasable on my device
+//gonna need to try a different approach.
+
+
+//thinking something along the lines of: 
+//work back from the incorrect bits, 
+//and find the gates that only affect incorrect bits
+
+//not sure what to do with the data that that will give me
+// mabye I make a map of which gates affect which bits
